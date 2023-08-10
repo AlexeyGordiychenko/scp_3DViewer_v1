7205f90f68abe6df6fb758ee301f568173252083
grapefru <cottagemad@gmail.com>
2023-08-10 21:34:19 +0700

    add models

--- src/tests/s21_test.c
+++ src/tests/s21_test.c
@@ -7,7 +7,7 @@
 START_TEST(viewer_tests_0) {
   s21_obj data_1 = {0, NULL, NULL};
   s21_obj data_2 = {0, NULL, NULL};
-  char file[] = "../../models/cube.obj";
+  char file[] = "tests/models/cube.txt";
   s21_parse_obj_file(file, &data_1);
   s21_parse_obj_file(file, &data_2);
   s21_xyz_movement(data_2.matrix_3d, 1, -0.25, 3.667);
@@ -26,7 +26,7 @@ END_TEST
 
 START_TEST(viewer_tests_1) {
   s21_obj data = {0, NULL, NULL};
-  char file[] = "../../models/cube.obj";
+  char file[] = "tests/models/cube.txt";
   double compared[8][3] = {
       {0.0, 0.0, 0.0}, {0.0, -1.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, -1.0, 1.0},
       {1.0, 0.0, 0.0}, {1.0, -1.0, 0.0}, {1.0, 0.0, 1.0}, {1.0, -1.0, 1.0},
@@ -46,7 +46,7 @@ END_TEST
 
 START_TEST(viewer_tests_2) {
   s21_obj data = {0, NULL, NULL};
-  char file[] = "../../models/cube.obj";
+  char file[] = "tests/models/cube.txt";
   double compared[8][3] = {
       {0.0, 0.0, 0.0},  {0.0, 0.0, -1.0},  {0.0, 1.0, 0.0},  {0.0, 1.0, -1.0},
       {-1.0, 0.0, 0.0}, {-1.0, 0.0, -1.0}, {-1.0, 1.0, 0.0}, {-1.0, 1.0, -1.0},
@@ -66,7 +66,7 @@ END_TEST
 
 START_TEST(viewer_tests_3) {
   s21_obj data = {0, NULL, NULL};
-  char file[] = "../../models/cube.obj";
+  char file[] = "tests/models/cube.txt";
   double compared[8][3] = {
       {0.0, 0.0, 0.0},
       {0.0, 0.0, 1.0},
@@ -92,7 +92,7 @@ END_TEST
 
 START_TEST(viewer_tests_4) {
   s21_obj data = {0, NULL, NULL};
-  char file[] = "../../models/cube.obj";
+  char file[] = "tests/models/cube.txt";
   double compared[8][3] = {
       {0.0, 0.0, 0.0}, {0.0, 0.0, 2.5}, {0.0, 2.5, 0.0}, {0.0, 2.5, 2.5},
       {2.5, 0.0, 0.0}, {2.5, 0.0, 2.5}, {2.5, 2.5, 0.0}, {2.5, 2.5, 2.5},
@@ -112,7 +112,7 @@ END_TEST
 
 START_TEST(viewer_tests_5) {
   s21_obj data = {0, NULL, NULL};
-  char file[] = "../../model/cube.obj";
+  char file[] = "tests/model/cube.txt";
   int error = s21_parse_obj_file(file, &data);
   ck_assert_int_eq(error, S21_ERR);
   s21_free_obj_struct(&data);
@@ -121,7 +121,7 @@ END_TEST
 
 START_TEST(viewer_tests_6) {
   s21_obj data = {0, NULL, NULL};
-  char file[] = "../../models/cube.obj";
+  char file[] = "tests/models/cube.txt";
   double compared_matrix[8][3] = {
       {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 1.0, 0.0}, {0.0, 1.0, 1.0},
       {1.0, 0.0, 0.0}, {1.0, 0.0, 1.0}, {1.0, 1.0, 0.0}, {1.0, 1.0, 1.0},
@@ -140,7 +140,7 @@ END_TEST
 
 START_TEST(viewer_tests_7) {
   s21_obj data = {0, NULL, NULL};
-  char file[] = "../../models/cube.obj";
+  char file[] = "tests/models/cube.txt";
   uint32_t compared_vertex[12][3] = {
       {1, 7, 5}, {1, 3, 7}, {1, 4, 3}, {1, 2, 4}, {3, 8, 7}, {3, 4, 8},
       {5, 7, 8}, {5, 8, 6}, {1, 5, 6}, {1, 6, 2}, {2, 6, 8}, {2, 8, 4},
@@ -159,7 +159,7 @@ END_TEST
 
 START_TEST(viewer_tests_8) {
   s21_obj data = {0, NULL, NULL};
-  char file[] = "../../models/flowers.obj";
+  char file[] = "tests/models/flowers.txt";
 
   s21_parse_obj_file(file, &data);
   ck_assert_int_eq(data.polygons_count, 4061);
@@ -169,7 +169,7 @@ END_TEST
 
 START_TEST(viewer_tests_9) {
   s21_obj data = {0, NULL, NULL};
-  char file[] = "../../models/cube1.obj";
+  char file[] = "tests/models/cube1.txt";
   int res = s21_parse_obj_file(file, &data);
   ck_assert_int_eq(res, 1);
 }
